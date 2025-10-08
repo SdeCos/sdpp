b88f06f7d7b87edc8b2d1fad9524028ec3881f29
SdeCos <saul.decossanchez@alumnos.uneatlantico.es>
2025-10-08 08:05:16 +0200

    primera solucin anadida

---
+++ PrimeraSolucion/ClassLibrary1/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    internal class Persona
+    {
+        public string Nombre = "Miguel";
+
+        public Persona()
+        {
+        }
+
+        public Persona(string nombre)
+        {
+            Nombre = nombre;
+        }
+    }
+}
---
+++ PrimeraSolucion/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Recoger un valor de al linea de comando
+            DateTime momentoActual = DateTime.Now;
+            string line = Console.ReadLine();
+            int elNumero = Convert.ToInt32(line);
+            Persona laPersona = new Persona("Marta");
+            Console.WriteLine(line.ToUpper());
+        }
+    }
+}
