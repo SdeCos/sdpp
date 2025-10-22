879e33f19b1b845c26e4f1f1261ed092a3e53972
SdeCos <saul.decossanchez@alumnos.uneatlantico.es>
2025-10-22 08:54:22 +0200

    Primer sistema distribuido con multithreading

---
+++ PrimerSistemaDistribuido/Servidor/ProcesadorCliente.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Servidor
+{
+    internal class ProcesadorCliente
+    {
+        private TcpClient cliente;
+        internal ProcesadorCliente(TcpClient ElCliente)
+        {
+            this.cliente = ElCliente;
+        }
+        internal void Procesar()
+        {
+            // Buffer for reading data
+            Byte[] bytes = new Byte[256];
+            string data = null;
+
+            Console.WriteLine("Connected!");
+
+            data = null;
+
+            // Get a stream object for reading and writing
+            NetworkStream stream = this.cliente.GetStream();
+
+            int i;
+
+            // Loop to receive all the data sent by the client.
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                // Translate data bytes to a ASCII string.
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                Console.WriteLine("Received: {0}", data);
+
+                Thread.Sleep(5000);
+                // Process the data sent by the client.
+                data = data.ToUpper();
+
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                // Send back a response.
+                stream.Write(msg, 0, msg.Length);
+                Console.WriteLine("Sent: {0}", data);
+            }
+            cliente.Close();
+        }
+    }
+}
---
+++ PrimerSistemaDistribuido/Servidor/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace Servidor
+{
+    class MyTcpListener
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Programa Servidor...");
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+
+                    ProcesadorCliente procesador = new ProcesadorCliente(client);
+                    ThreadStart ts = new ThreadStart(procesador.Procesar);
+                    Thread hilo = new Thread(ts);
+                    hilo.Start();
+
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                server.Stop();
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+    }
+}
+
