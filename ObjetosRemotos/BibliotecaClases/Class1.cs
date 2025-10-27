f25bfcba239571921219911629dd75e6c77ef1ca
SdeCos <saul.decossanchez@alumnos.uneatlantico.es>
2025-10-27 08:59:33 +0100

    Objetos remotos, con biblioteca de clases

---
+++ ObjetosRemotos/BibliotecaClases/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaClases
+{
+     public interface MovieTicketInterface
+     {
+         string GetTicketStatus(string stringToPrint);
+     }
+     public class MovieTicket : MarshalByRefObject, MovieTicketInterface
+     {
+         public string GetTicketStatus(string stringToPrint)
+         {
+             string returnStatus = "Ticket Confirmed";
+             Console.WriteLine("Enquiry for {0}", stringToPrint);
+             Console.WriteLine("Sending back status: {0}", returnStatus);
+             return returnStatus;
+         }
+     }
+}
---
+++ ObjetosRemotos/Servidor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BibliotecaClases;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servidor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TicketServer();
+        }
+        static void TicketServer()
+        {
+            Console.WriteLine("Ticket Server started...");
+            TcpChannel tcpChannel = new TcpChannel(9998);
+            ChannelServices.RegisterChannel(tcpChannel);
+            Type commonInterfaceType = typeof(MovieTicket);
+            //Type commonInterfaceType = Type.GetType("MovieTicket");
+            RemotingConfiguration.RegisterWellKnownServiceType(commonInterfaceType,
+            "MovieTicketBooking", WellKnownObjectMode.SingleCall);
+            System.Console.WriteLine("Press ENTER to quitnn");
+            System.Console.ReadLine();
+        }
+
+
+    }
+}
